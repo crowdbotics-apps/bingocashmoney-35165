21b46b80206084720f9a9cabc577ba29d16b604f
Crowdbotics <team@crowdbotics.com>
2022-05-10 23:24:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bingocashmoney_35165/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bingocashmoney_35165;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bingocashmoney_35165";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bingocashmoney_35165",
+  url: "https://bingocashmoney_35165.botics.co",
+  api: "https://bingocashmoney_35165.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
